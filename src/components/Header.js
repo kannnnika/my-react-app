8a22e0b6841e8466b334d6becb152ef405bf4b68
kannnnika <63050579@go.buu.ac.th>
2023-10-30 08:21:14 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/logo512.png" width="30px" />
+      <h4>React Website</h4>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const content = () => {
+  return (
+    <div className="card">
+      <img
+        width="100%"
+        src="https://images.unsplash.com/photo-1698308246287-e620bfbd62ec?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div>
+        <h6>The Sea</h6>
+      </div>
+    </div>
+  );
+};
+
+export default content;
